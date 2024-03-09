03c4d38d3bcf59a50858c2c62127014c6016b0f5
Flavio Dias <flavio.goncalvesdias@gmail.com>
2024-03-09 13:55:06 -0300

    Teste Unitario ex11

--- qae2e_testesUnitarios/src/main/java/easy/Exercicio11_easy.java
+++ qae2e_testesUnitarios/src/main/java/easy/Exercicio11_easy.java
@@ -4,11 +4,15 @@ import javax.swing.*;
 
 public class Exercicio11_easy {
 
-    public void somaDoisAoNumeroDigitado(int numeroDigitado) {
+    public int[] somaDoisAoNumeroDigitado(int numeroDigitado) {
+        int[] somaDois = new int[100];
         for (int i = 0; i <= 99; i++) {
             numeroDigitado = numeroDigitado + 2;
             System.out.println("Iteração: " + i + " - Valor: " + numeroDigitado);
+            somaDois[i] = numeroDigitado;
+
         }
         System.out.println("Fim das iterações!!!");
+        return somaDois;
     }
 }
---
+++ qae2e_testesUnitarios/src/test/java/TesteUnitarioEx11.java
@@ -0,0 +1,17 @@
+import easy.Exercicio11_easy;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteUnitarioEx11 {
+
+
+    @Test
+    public void testSomaDois(){
+        int [] somaDoisAssert = {3, 5, 7};
+        Exercicio11_easy exercicio11Easy = new Exercicio11_easy();
+        int [] valorAtual = exercicio11Easy.somaDoisAoNumeroDigitado(1);
+        for (int i = 0; i < somaDoisAssert.length; i++) {
+            Assert.assertEquals(somaDoisAssert[i], valorAtual[i]);
+        }
+    }
+}

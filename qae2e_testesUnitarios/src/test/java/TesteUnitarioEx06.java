c0bdf27c766890786628fed244000deca82d2872
Flavio Dias <flavio.goncalvesdias@gmail.com>
2024-03-09 10:57:05 -0300

    Teste unitario ex06

---
+++ qae2e_testesUnitarios/src/test/java/TesteUnitarioEx06.java
@@ -0,0 +1,17 @@
+import easy.Exercicio06_easy;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteUnitarioEx06 {
+
+
+    @Test
+    public void verificarAprovacaoAluno() {
+        Exercicio06_easy exercicio06Easy = new Exercicio06_easy();
+        Assert.assertEquals("Você está aprovado!!!", exercicio06Easy.verificarAprovacaoAluno(5, 6));
+        Assert.assertEquals("Você está reprovado!!!", exercicio06Easy.verificarAprovacaoAluno(4, 4));
+        Assert.assertEquals("Você está de exame!!!", exercicio06Easy.verificarAprovacaoAluno(5, 5));
+
+
+    }
+}

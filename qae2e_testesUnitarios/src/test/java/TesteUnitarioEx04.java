b7ea7ddcd7837c1ef0aa3fcc9f317d8be899e6dc
Flavio Dias <flavio.goncalvesdias@gmail.com>
2024-03-09 10:00:28 -0300

    Teste unitario ex04 easy

---
+++ qae2e_testesUnitarios/src/test/java/TesteUnitarioEx04.java
@@ -0,0 +1,12 @@
+import easy.Exercicio04_easy;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteUnitarioEx04 {
+
+    @Test
+    public void testeCalcularDobro() {
+        Exercicio04_easy exercicio04Easy = new Exercicio04_easy();
+        Assert.assertEquals("4", exercicio04Easy.calcularDobro(2).toString());
+    }
+}
